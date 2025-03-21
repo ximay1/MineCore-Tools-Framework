0e40b2b1fe1f4a7727ea177ef81e57cfd8081b68
ximaydev <ximayek@gmail.com>
2025-03-21 20:48:27 +0100

    Inventory implemented

--- Plugins/MineCore/Source/MineCore/Private/Components/MC_InventoryComponent.cpp
+++ Plugins/MineCore/Source/MineCore/Private/Components/MC_InventoryComponent.cpp
@@ -1,10 +1,132 @@
 #include "Components/MC_InventoryComponent.h"
+#include "MC_LogChannels.h"
 
-UMC_InventoryComponent::UMC_InventoryComponent()
+UMC_InventoryComponent::UMC_InventoryComponent() : MaxSlots(40)
 {
 	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UMC_InventoryComponent::CreateInventory()
 {
+	//TODO: Create Inventory
+}
+
+void UMC_InventoryComponent::RefreshInventoryWidget()
+{
+	/*if (InventoryWidget)
+	{
+		TODO: InventoryWidget->RefreshWidget()
+	}*/
+}
+
+void UMC_InventoryComponent::DropItem(uint8 Slot)
+{
+	//TODO: Create a bag at player's location
+}
+
+void UMC_InventoryComponent::DropItem(UMC_Item* Item)
+{
+	//TODO: Create a bag at player's location
+}
+
+bool UMC_InventoryComponent::FindValidSlot(uint8& OutSlot)
+{
+	// Iterate through all the slots in the inventory
+	for (uint8 CurrentSlot = 0; CurrentSlot < MaxSlots; CurrentSlot++)
+	{
+		// Check if the current slot is empty (does not contain an item)
+		if (!Items.Contains(CurrentSlot))
+		{
+			OutSlot = CurrentSlot; 	// Assign the found empty slot to OutSlot
+			return true;			// Return true since a valid (empty) slot was found
+		}
+	}
+
+	// Return false if no empty slot was found
+	return false;
+}
+
+void UMC_InventoryComponent::AddItemToInventory_Implementation(uint8 Slot, UMC_Item* Item)
+{
+#if !UE_BUILD_SHIPPING
+	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
+	if (Slot > MaxSlots)
+	{
+		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+	if (Item == nullptr)
+	{
+		UE_LOGFMT(LogInventory, Error, "Item is nullptr. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+#endif
+
+	//Check if there is something on that slot
+	if (GetItemFromInventory(Slot) != nullptr)
+	{
+		//Find next valid slot
+		if (FindValidSlot(Slot))
+		{
+			//Add to the inventory
+			Items.Add(Slot, Item);
+		}
+		else
+		{
+			// Drop the item from the inventory using the given item
+			DropItem(Item);
+			
+			UE_LOGFMT(LogInventory, Log, "We need to create a bag of items at the player's location when attempting to add an item to the inventory");
+		}
+		
+		// Refresh the inventory widget to reflect the changes
+		RefreshInventoryWidget();
+	}
+}
+
+
+void UMC_InventoryComponent::RemoveItemFromInventory_Implementation(uint8 Slot, EItemAction ItemAction)
+{
+#if !UE_BUILD_SHIPPING
+	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
+	if (Slot > MaxSlots)
+	{
+		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+#endif
+
+	// Perform the assigned action based on the ItemAction enum
+	switch (ItemAction)
+	{
+		case EItemAction::IA_Destroy:
+			{
+				// Find the item in the inventory map using the given slot
+				if (UMC_Item* ItemToDestroy = Items.FindRef(Slot))
+				{
+					// Start destroying the item by calling BeginDestroy()
+					ItemToDestroy->BeginDestroy();
+
+					// Remove the item from the inventory map
+					Items.Remove(Slot);
+
+					// Refresh the inventory widget to reflect the changes
+					RefreshInventoryWidget();
+				}
+				else
+				{
+					// Log an error if the item is nullptr (doesn't exist) in the inventory
+					UE_LOGFMT(LogInventory, Error, "We wanted to destroy the item which doesn't exist in the inventory, we passed an invalid slot. File - {0}, Line - {1}", __FILE__, __LINE__);
+				}
+
+				break;
+			}
+		case EItemAction::IA_Drop:
+			{
+				// Drop the item from the inventory using the given slot
+				DropItem(Slot);
+				
+				break;
+			}
+	};
+
+	// Refresh the inventory widget to reflect the changes
+	RefreshInventoryWidget();
 }
--- Plugins/MineCore/Source/MineCore/Public/Components/MC_InventoryComponent.h
+++ Plugins/MineCore/Source/MineCore/Public/Components/MC_InventoryComponent.h
@@ -5,11 +5,20 @@
 #include "Items/MC_Item.h"
 #include "MC_InventoryComponent.generated.h"
 
+UENUM(BlueprintType)
+enum class EItemAction : uint8
+{
+	IA_Destroy UMETA(DisplayName = "Destroy"),  // Player wants to destroy the item
+	IA_Drop    UMETA(DisplayName = "Drop"),     // Player wants to drop the item
+};
+
 UCLASS( ClassGroup=(MiningSystem), meta=(BlueprintSpawnableComponent) )
 class MINECORE_API UMC_InventoryComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	// TODO: Change UUserWidget class to the base InventoryWidget class
+	
 public:	
 	// Sets default values for this component's properties
 	UMC_InventoryComponent();
@@ -18,13 +27,47 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Inventory Component")
 	void CreateInventory();
 
+	/** Refresh Widget Inventory */
+	void RefreshInventoryWidget();
+	
+	/** Adds an item to the inventory */
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "Inventory Component")
+	void AddItemToInventory(uint8 Slot, UMC_Item* Item);
+
+	/** Removes an item from the inventory */
+	UFUNCTION(Server, Reliable, BlueprintCallable, Category = "Inventory Component")
+	void RemoveItemFromInventory(uint8 Slot, EItemAction ItemAction);
+
+	/** Checks if an item exists in the inventory. Returns UMC_Item if found, otherwise nullptr */
+	UFUNCTION(BlueprintCallable, Category = "Inventory Component")
+	FORCEINLINE UMC_Item* GetItemFromInventory(uint8 Slot) { return Items.FindRef(Slot); }
+
+	/** Drops the item as a bag at the player's location */
+	void DropItem(uint8 Slot);
+	void DropItem(UMC_Item* Item);
+	
+	/** Finds the first available (empty) slot in the inventory. Returns true if a valid slot is found, otherwise false. */
+	UFUNCTION(BlueprintCallable, Category = "Inventory Component")
+	bool FindValidSlot(uint8& OutSlot);
+	
+	/** Get Max Slots */
+	UFUNCTION(BlueprintGetter, Category = "Inventory Component")
+	FORCEINLINE uint8 GetMaxSlots() const { return MaxSlots; }
+
 protected:
-	// TODO: Change UUserWidget class to the base inventory widget class
 	/** Widget class representing the Inventory */ 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Inventory Component")
 	TSubclassOf<UUserWidget> InventoryClass;
 
-	/** Map of items in the inventory. uint8 represents the number of slots. */
+	/** Map of items in the inventory. uint8 represents a number of slot. */
 	UPROPERTY(BlueprintReadOnly, Category = "Inventory Component")
 	TMap<uint8, TObjectPtr<UMC_Item>> Items;
+
+	/** Cached Pointer to the Inventory widget */
+	 UPROPERTY(BlueprintReadOnly, Category = "Inventory Component")
+	TObjectPtr<UUserWidget> InventoryWidget;
+
+	/** Maximum number of slots in the inventory */
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory Component", meta = (AllowPrivateAccess))
+	uint8 MaxSlots;
 };
