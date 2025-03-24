eb74d564a5f6ef50cf87add99413a226aede6f47
ximaydev <ximayek@gmail.com>
2025-03-24 18:32:50 +0100

    MC_UsuableItemConfig class added

---
+++ Plugins/MineCore/Source/MineCore/Public/Data/Item/MC_UsableItemConfig.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MC_ItemConfig.h"
+#include "MC_UsableItemConfig.generated.h"
+
+UCLASS()
+class MINECORE_API UMC_UsableItemConfig : public UMC_ItemConfig
+{
+	GENERATED_BODY()
+};
