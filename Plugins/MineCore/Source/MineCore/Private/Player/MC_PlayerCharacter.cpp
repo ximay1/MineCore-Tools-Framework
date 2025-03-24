5e4f4322381076a1d4090e25ca15b00c6e1b0446
ximaydev <ximayek@gmail.com>
2025-03-24 17:38:17 +0100

    MiningSystemComponent && Inventory Component added to the player class

--- Plugins/MineCore/Source/MineCore/Private/Player/MC_PlayerCharacter.cpp
+++ Plugins/MineCore/Source/MineCore/Private/Player/MC_PlayerCharacter.cpp
@@ -1,9 +1,17 @@
 #include "Player/MC_PlayerCharacter.h"
+#include "Components/MC_InventoryComponent.h"
+#include "Components/MC_MiningSystemComponent.h"
 
 AMC_PlayerCharacter::AMC_PlayerCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	//Create Inventory Component
+	InventoryComponent = CreateDefaultSubobject<UMC_InventoryComponent>(FName("Inventory Component"));
+
+	//Create Mining System Component
+	MiningSystemComponent = CreateDefaultSubobject<UMC_MiningSystemComponent>(FName("Mining System Component"));
 }
 
 void AMC_PlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
--- Plugins/MineCore/Source/MineCore/Public/Player/MC_PlayerCharacter.h
+++ Plugins/MineCore/Source/MineCore/Public/Player/MC_PlayerCharacter.h
@@ -4,6 +4,9 @@
 #include "GameFramework/Character.h"
 #include "MC_PlayerCharacter.generated.h"
 
+class UMC_InventoryComponent;
+class UMC_MiningSystemComponent;
+
 UCLASS()
 class MINECORE_API AMC_PlayerCharacter : public ACharacter
 {
@@ -15,4 +18,21 @@ public:
 	
 	/** Events */
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Get Inventory Component */
+	UFUNCTION(BlueprintCallable, Category = "Inventory Component")
+	FORCEINLINE UMC_InventoryComponent* GetInventoryComponent() const { return InventoryComponent; } 
+
+	/** Get Mining System Component */
+	UFUNCTION(BlueprintCallable, Category = "Mining System Component")
+	FORCEINLINE UMC_MiningSystemComponent* GetMiningSystemComponent() const { return MiningSystemComponent; }
+	
+protected:
+	/** Inventory Component */
+	UPROPERTY(BlueprintReadOnly, Category = "Inventory Component")
+	TObjectPtr<UMC_InventoryComponent> InventoryComponent;
+
+	/** Mining System Component */
+	UPROPERTY(BlueprintReadOnly, Category = "Mining System Component")
+	TObjectPtr<UMC_MiningSystemComponent> MiningSystemComponent;
 };
