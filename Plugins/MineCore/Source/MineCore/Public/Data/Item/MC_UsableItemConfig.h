013dfbec23e206e9364e12b3433568d1c049c5ca
ximaydev <ximayek@gmail.com>
2025-03-24 18:40:11 +0100

    Created definiton for UMC_UsuableItemConfig.h

--- Plugins/MineCore/Source/MineCore/Public/Data/Item/MC_UsableItemConfig.h
+++ Plugins/MineCore/Source/MineCore/Public/Data/Item/MC_UsableItemConfig.h
@@ -4,8 +4,14 @@
 #include "MC_ItemConfig.h"
 #include "MC_UsableItemConfig.generated.h"
 
+/** Base class for usable items that have durability, such as weapons, tools, and armors. */
 UCLASS()
 class MINECORE_API UMC_UsableItemConfig : public UMC_ItemConfig
 {
 	GENERATED_BODY()
+
+public:
+	/** The maximum durability the item can have. Must be greater than 0. */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Usable Item", meta = (ClampMin = "0.01"))
+	float MaxDurability = 1.0f;
 };
