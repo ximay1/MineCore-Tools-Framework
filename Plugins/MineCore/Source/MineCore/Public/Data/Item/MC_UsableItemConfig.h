39ae5c135f5fd6981f7960d18a8a61bfcc08286e
ximaydev <ximayek@gmail.com>
2025-03-25 17:39:21 +0100

    .

--- Plugins/MineCore/Source/MineCore/Public/Data/Item/MC_UsableItemConfig.h
+++ Plugins/MineCore/Source/MineCore/Public/Data/Item/MC_UsableItemConfig.h
@@ -11,6 +11,8 @@ class MINECORE_API UMC_UsableItemConfig : public UMC_ItemConfig
 	GENERATED_BODY()
 
 public:
+	FORCEINLINE virtual FPrimaryAssetId GetPrimaryAssetId() const override { return FPrimaryAssetId("UsableItemConfig", GetFName()); }
+
 	/** The maximum durability the item can have. Must be greater than 0. */
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Usable Item", meta = (ClampMin = "0.01"))
 	float MaxDurability = 1.0f;
