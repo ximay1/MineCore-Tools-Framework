3a0be769559c2b856f6ad42d6b4fdefdbde42329
ximaydev <ximayek@gmail.com>
2025-03-26 22:21:59 +0100

    Corrections

--- Plugins/MineCore/Source/MineCore/Private/Components/MC_InventoryComponent.cpp
+++ Plugins/MineCore/Source/MineCore/Private/Components/MC_InventoryComponent.cpp
@@ -50,6 +50,96 @@ void UMC_InventoryComponent::RefreshInventoryWidget()
 	}*/
 }
 
+void UMC_InventoryComponent::AddItemToInventory_Implementation(uint8 Slot, UMC_Item* Item)
+{
+#if !UE_BUILD_SHIPPING
+	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
+	if (Slot > MaxSlots)
+	{
+		//Log Error
+		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+	if (Item == nullptr)
+	{
+		//Log Error
+		UE_LOGFMT(LogInventory, Error, "Item is nullptr. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+#endif
+
+	//Check if there is something on that slot
+	if (GetItemFromInventory(Slot) != nullptr)
+	{
+		//Find next valid slot
+		if (FindValidSlot(Slot))
+		{
+			//Add to the inventory
+			Items.Add(Slot, Item);
+
+			//Call delegate
+			OnItemAddedToInventory.Broadcast(Slot, Item);
+		}
+		else
+		{
+			// Drop the item from the inventory using the given item
+			DropItem(Item);
+			
+			UE_LOGFMT(LogInventory, Log, "We need to create a bag of items at the player's location when attempting to add an item to the inventory");
+		}
+		
+		// Refresh the inventory widget to reflect the changes
+		RefreshInventoryWidget();
+	}
+}
+
+void UMC_InventoryComponent::RemoveItemFromInventory_Implementation(uint8 Slot, EItemAction ItemAction)
+{
+#if !UE_BUILD_SHIPPING
+	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
+	if (Slot > MaxSlots)
+	{
+		//Log Error
+		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
+	}
+#endif
+
+	// Perform the assigned action based on the ItemAction enum
+	switch (ItemAction)
+	{
+		case EItemAction::Destroy:
+			{
+				// Find the item in the inventory map using the given slot
+				if (UMC_Item* ItemToDestroy = Items.FindRef(Slot))
+				{
+					// Start destroying the item by calling BeginDestroy()
+					ItemToDestroy->BeginDestroy();
+
+					// Remove the item from the inventory map
+					Items.Remove(Slot);
+
+					// Refresh the inventory widget to reflect the changes
+					RefreshInventoryWidget();
+				}
+				else
+				{
+					// Log Error
+					UE_LOGFMT(LogInventory, Error, "We wanted to destroy the item which doesn't exist in the inventory, we passed an invalid slot. File - {0}, Line - {1}", __FILE__, __LINE__);
+				}
+
+				break;
+			}
+		case EItemAction::Drop:
+			{
+				// Drop the item from the inventory using the given slot
+				DropItem(Slot);
+				
+				break;
+			}
+	};
+
+	// Refresh the inventory widget to reflect the changes
+	RefreshInventoryWidget();
+}
+
 void UMC_InventoryComponent::FindItemsByFilter(const FInventoryItemFilter& InventoryItemFilter, TArray<UMC_Item*>& OutItems) const
 {
 #if !UE_BUILD_SHIPPING
@@ -177,95 +267,4 @@ bool UMC_InventoryComponent::FindValidSlot(uint8& OutSlot) const
 
 	// Return false if no empty slot was found
 	return false;
-}
-
-void UMC_InventoryComponent::AddItemToInventory_Implementation(uint8 Slot, UMC_Item* Item)
-{
-#if !UE_BUILD_SHIPPING
-	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
-	if (Slot > MaxSlots)
-	{
-		//Log Error
-		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
-	}
-	if (Item == nullptr)
-	{
-		//Log Error
-		UE_LOGFMT(LogInventory, Error, "Item is nullptr. File - {0}, Line - {1}.", __FILE__, __LINE__);
-	}
-#endif
-
-	//Check if there is something on that slot
-	if (GetItemFromInventory(Slot) != nullptr)
-	{
-		//Find next valid slot
-		if (FindValidSlot(Slot))
-		{
-			//Add to the inventory
-			Items.Add(Slot, Item);
-
-			//Call delegate
-			OnItemAddedToInventory.Broadcast(Slot, Item);
-		}
-		else
-		{
-			// Drop the item from the inventory using the given item
-			DropItem(Item);
-			
-			UE_LOGFMT(LogInventory, Log, "We need to create a bag of items at the player's location when attempting to add an item to the inventory");
-		}
-		
-		// Refresh the inventory widget to reflect the changes
-		RefreshInventoryWidget();
-	}
-}
-
-
-void UMC_InventoryComponent::RemoveItemFromInventory_Implementation(uint8 Slot, EItemAction ItemAction)
-{
-#if !UE_BUILD_SHIPPING
-	// Check if the slot number is valid (Slot should be less than or equal to MaxSlots)
-	if (Slot > MaxSlots)
-	{
-		//Log Error
-		UE_LOGFMT(LogInventory, Error, "Invalid slot number. Slot exceeds MaxSlots. File - {0}, Line - {1}.", __FILE__, __LINE__);
-	}
-#endif
-
-	// Perform the assigned action based on the ItemAction enum
-	switch (ItemAction)
-	{
-		case EItemAction::Destroy:
-			{
-				// Find the item in the inventory map using the given slot
-				if (UMC_Item* ItemToDestroy = Items.FindRef(Slot))
-				{
-					// Start destroying the item by calling BeginDestroy()
-					ItemToDestroy->BeginDestroy();
-
-					// Remove the item from the inventory map
-					Items.Remove(Slot);
-
-					// Refresh the inventory widget to reflect the changes
-					RefreshInventoryWidget();
-				}
-				else
-				{
-					// Log Error
-					UE_LOGFMT(LogInventory, Error, "We wanted to destroy the item which doesn't exist in the inventory, we passed an invalid slot. File - {0}, Line - {1}", __FILE__, __LINE__);
-				}
-
-				break;
-			}
-		case EItemAction::Drop:
-			{
-				// Drop the item from the inventory using the given slot
-				DropItem(Slot);
-				
-				break;
-			}
-	};
-
-	// Refresh the inventory widget to reflect the changes
-	RefreshInventoryWidget();
 }
