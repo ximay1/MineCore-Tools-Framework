1034a83fdf3e9599399c2306f740b373fc6a68ee
ximaydev <ximayek@gmail.com>
2025-04-07 20:42:29 +0200

    MC_ItemBag created

---
+++ Plugins/MineCore/Source/MineCore/Private/Actors/Bags/MC_ItemBag.cpp
@@ -0,0 +1,7 @@
+#include "Actors/Bags/MC_ItemBag.h"
+
+AMC_ItemBag::AMC_ItemBag()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
---
+++ Plugins/MineCore/Source/MineCore/Public/Actors/Bags/MC_ItemBag.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MC_ItemBag.generated.h"
+
+UCLASS()
+class MINECORE_API AMC_ItemBag : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	/** Constructor */
+	AMC_ItemBag();
+};
