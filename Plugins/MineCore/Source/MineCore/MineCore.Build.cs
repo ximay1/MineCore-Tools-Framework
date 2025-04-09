2ca638c07d55ca185cf34bf0770c0ff9b0a9254e
ximaydev <ximayek@gmail.com>
2025-04-09 18:14:54 +0200

    AssetRegistry added to build.cs

--- Plugins/MineCore/Source/MineCore/MineCore.Build.cs
+++ Plugins/MineCore/Source/MineCore/MineCore.Build.cs
@@ -40,7 +40,8 @@ public class MineCore : ModuleRules
 				"SlateCore",
 				"UMG",
 				"Json",
-				"JsonUtilities"
+				"JsonUtilities",
+				"AssetRegistry"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
