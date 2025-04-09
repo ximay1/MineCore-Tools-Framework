25deaf93330a7d434503b5e598c8ecf434028901
ximaydev <ximayek@gmail.com>
2025-04-09 19:00:10 +0200

    Properties added

--- Plugins/MineCore/Source/MineCore/Public/DeveloperSettings/MC_DeveloperSettings.h
+++ Plugins/MineCore/Source/MineCore/Public/DeveloperSettings/MC_DeveloperSettings.h
@@ -4,7 +4,7 @@
 #include "Engine/DeveloperSettings.h"
 #include "MC_DeveloperSettings.generated.h"
 
-UCLASS(Config = game, DefaultConfig)
+UCLASS(Config = Game, DefaultConfig)
 class MINECORE_API UMC_DeveloperSettings : public UDeveloperSettings
 {
 	GENERATED_BODY()
@@ -19,4 +19,12 @@ public:
 	virtual FText GetSectionDescription() const override;
 #endif
 	//~ End UDeveloperSettings interface
+
+	/** Path to the directory containing all Primary Data Assets that inherit from UMC_DT_ItemConfig class. */
+	UPROPERTY(Config, EditAnywhere, Category = "Mine Core Settings")
+	FString PathToItemsPrimaryDataAssets;
+
+	/** Defines a custom PrimaryAssetType for item classification. When empty (default), uses the PrimaryAssetType defined in UMC_DT_ItemConfig class. */
+	UPROPERTY(Config, EditAnywhere, Category = "Mine Core Settings")
+	FPrimaryAssetType PrimaryAssetType;
 };
