8d4dd0f9ac8b1e10345df060dfc22a623f525a29
ximaydev <ximayek@gmail.com>
2025-04-13 19:47:46 +0200

    Correction

--- Plugins/MineCore/Source/MineCoreUI/Public/Game/Inventory/MCUI_Inventory.h
+++ Plugins/MineCore/Source/MineCoreUI/Public/Game/Inventory/MCUI_Inventory.h
@@ -17,7 +17,7 @@ protected:
 	
 	/** Caches all inventory slot widgets matching the naming pattern: [InventorySlotName]_[Index]. */
 	UFUNCTION(BlueprintCallable, Category = "Inventory Widget")
-	void CacheInventorySlots(UMC_InventoryComponent* InventoryComponent);
+	virtual void CacheInventorySlots(UMC_InventoryComponent* InventoryComponent);
 	
 	/**
 	 * Base name for inventory slot widgets (without the numeric suffix).
