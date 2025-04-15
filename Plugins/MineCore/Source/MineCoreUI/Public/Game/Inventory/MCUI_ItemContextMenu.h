1636b55040e27662858637293d3898725d137c5a
ximaydev <ximayek@gmail.com>
2025-04-15 20:20:36 +0200

    MCUI_ItemContextMenu created

---
+++ Plugins/MineCore/Source/MineCoreUI/Public/Game/Inventory/MCUI_ItemContextMenu.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "MCUI_ItemContextMenu.generated.h"
+
+UCLASS()
+class MINECOREUI_API UMCUI_ItemContextMenu : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+	
+};
