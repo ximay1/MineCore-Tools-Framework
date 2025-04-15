693e4afa77ea76ad7d225a93abefaad33c7f9b66
ximaydev <ximayek@gmail.com>
2025-04-15 20:56:26 +0200

    Widgets binded

--- Plugins/MineCore/Source/MineCoreUI/Private/Game/Inventory/MCUI_ItemContextMenu.cpp
+++ Plugins/MineCore/Source/MineCoreUI/Private/Game/Inventory/MCUI_ItemContextMenu.cpp
@@ -1,2 +1,15 @@
 #include "Game/Inventory/MCUI_ItemContextMenu.h"
 
+#include "Components/ScaleBox.h"
+
+void UMCUI_ItemContextMenu::InitializeItemContextMenu(UMC_DT_ItemConfig* Item)
+{
+}
+
+void UMCUI_ItemContextMenu::CreateDescription(const FText& Description)
+{
+	//Create scale box
+	UScaleBox* ScaleBox_Description = NewObject<UScaleBox>();
+
+	//Create Common 
+}
--- Plugins/MineCore/Source/MineCoreUI/Public/Game/Inventory/MCUI_ItemContextMenu.h
+++ Plugins/MineCore/Source/MineCoreUI/Public/Game/Inventory/MCUI_ItemContextMenu.h
@@ -6,6 +6,10 @@
 
 /** Forward Declarations */
 class UMC_DT_ItemConfig;
+class UVerticalBox;
+class UCommonTextBlock;
+class UCommonTextStyle;
+class UCommonTextScrollStyle;
 
 UCLASS()
 class MINECOREUI_API UMCUI_ItemContextMenu : public UCommonActivatableWidget
@@ -16,4 +20,24 @@ public:
 	/** Initializes this Item Context Menu Widget with the specified item */
 	UFUNCTION(BlueprintCallable, Category = "Inventory Slot Widget")
 	virtual void InitializeItemContextMenu(UMC_DT_ItemConfig* Item);
+
+	/** Common Text Block Description Style */
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory Slot Widget")
+	TSubclassOf<UCommonTextStyle> CommonTextDescriptonStyle_Class;
+
+	/** Common Text Block Description Scroll Style */
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory Slot Widget")
+	TSubclassOf<UCommonTextScrollStyle> CommonTextDescriptonScrollStyle_Class; 
+	
+protected:
+	/** Bind Widgets */
+	/** Vertical Boxes */
+	UPROPERTY(BlueprintReadOnly, Category = "Item Context Menu", meta = (BindWidget))
+	TObjectPtr<UVerticalBox> VerticalBox_Stats;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Item Context Menu", meta = (BindWidget))
+	TObjectPtr<UVerticalBox> VerticalBox_Description;
+
+	/** Create Description for this widget */
+	void CreateDescription(const FText& Description);
 };
