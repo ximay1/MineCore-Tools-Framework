19eed7d032ed5f501e42b3d138f5bd4e4917961f
ximaydev <ximayek@gmail.com>
2025-04-18 00:44:03 +0200

    code refactoring

--- Plugins/MineCore/Source/MineCore/Public/MC_LogChannels.h
+++ Plugins/MineCore/Source/MineCore/Public/MC_LogChannels.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Logging/LogMacros.h"
-#include "Logging/StructuredLog.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogResourceNode, Log, All);
 DECLARE_LOG_CATEGORY_EXTERN(LogInventory, Log, All);
