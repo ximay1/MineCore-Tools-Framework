6053c128a6ea1427dd17a4f5edb7735f1cc21827
ximaydev <ximayek@gmail.com>
2025-04-18 16:48:41 +0200

    Corrections

--- Plugins/MineCore/Source/MineCoreUI/Private/Layouts/MCUI_Game_Layout.cpp
+++ Plugins/MineCore/Source/MineCoreUI/Private/Layouts/MCUI_Game_Layout.cpp
@@ -1,12 +1,14 @@
 #include "Layouts/MCUI_Game_Layout.h"
 #include "CommonUI/Public/Widgets/CommonActivatableWidgetContainer.h"
+#include "Game/Inventory/MCUI_Inventory.h"
+#include "Game/Inventory/MCUI_ItemContextMenu.h"
 
-void UMCUI_Game_Layout::PushInventory(const TSubclassOf<UCommonActivatableWidget> WidgetClass) const
+UMCUI_Inventory*  UMCUI_Game_Layout::PushInventory(const TSubclassOf<UCommonActivatableWidget> WidgetClass) const
 {
-	CAWS_Inventory->AddWidget(WidgetClass);
+	return Cast<UMCUI_Inventory>(CAWS_Inventory->AddWidget(WidgetClass));
 }
 
-void UMCUI_Game_Layout::PushItemContextMenu(const TSubclassOf<UCommonActivatableWidget> WidgetClass) const
+UMCUI_ItemContextMenu* UMCUI_Game_Layout::PushItemContextMenu(const TSubclassOf<UCommonActivatableWidget> WidgetClass) const
 {
-	CAWS_ItemContextInfo->AddWidget(WidgetClass);
+	return Cast<UMCUI_ItemContextMenu>(CAWS_ItemContextInfo->AddWidget(WidgetClass));
 }
--- Plugins/MineCore/Source/MineCoreUI/Public/Layouts/MCUI_Game_Layout.h
+++ Plugins/MineCore/Source/MineCoreUI/Public/Layouts/MCUI_Game_Layout.h
@@ -6,6 +6,8 @@
 
 /** Forward Declarations */
 class UCommonActivatableWidgetStack;
+class UMCUI_ItemContextMenu;
+class UMCUI_Inventory;
 
 /** Main Game Layout Widget managing core UI stacks and navigation. */
 UCLASS()
@@ -16,11 +18,11 @@ class MINECOREUI_API UMCUI_Game_Layout : public UCommonActivatableWidget
 public:
 	/** Push Inventory */
 	UFUNCTION(BlueprintCallable, Category = "Game Layout")
-	void PushInventory(UPARAM(meta = (AllowedClasses = "MCUI_Inventory")) TSubclassOf<UCommonActivatableWidget> WidgetClass) const;
+	UMCUI_Inventory* PushInventory(UPARAM(meta = (AllowedClasses = "MCUI_Inventory")) TSubclassOf<UCommonActivatableWidget> WidgetClass) const;
 
 	/** Push Items Info */
 	UFUNCTION(BlueprintCallable, Category = "Game Layout")
-	void PushItemContextMenu(UPARAM(meta = (AllowedClasses = "MCUI_ItemContextMenu")) TSubclassOf<UCommonActivatableWidget> WidgetClass) const;
+	UMCUI_ItemContextMenu* PushItemContextMenu(UPARAM(meta = (AllowedClasses = "MCUI_ItemContextMenu")) TSubclassOf<UCommonActivatableWidget> WidgetClass) const;
 	
 protected:
 	/** Bind Widgets */
