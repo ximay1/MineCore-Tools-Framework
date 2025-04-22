235327d250c1ccfe6ef4ccc356bcaa16c15fe896
ximaydev <ximayek@gmail.com>
2025-04-22 22:41:01 +0200

    MineCoreLibrary created

---
+++ Plugins/MineCore/Source/MineCore/Private/MineCoreLibrary.cpp
@@ -0,0 +1,1 @@
+#include "MineCoreLibrary.h"
---
+++ Plugins/MineCore/Source/MineCore/Public/MineCoreLibrary.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "MineCoreLibrary.generated.h"
+
+UCLASS()
+class MINECORE_API UMineCoreLibrary : public UBlueprintFunctionLibrary
+{
+	
+};
